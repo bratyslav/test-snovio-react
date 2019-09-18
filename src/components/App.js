a906f4600cefa21edc59f7e163a956ab9e1c4614
bratyslav <beginner.altavista@gmail.com>
2019-09-18 17:46:17 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="test__mytest">My React AppA!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
