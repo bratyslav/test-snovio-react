5851eabcb0fce43ab131564929a22a862996263f
bratyslav <beginner.altavista@gmail.com>
2019-09-19 19:10:33 +0300

    finish animation

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Loader = () => {
+  const [itemClassName, setItemClassName] = useState('');
+
+  const timeout = setTimeout(() => {
+    setItemClassName('-loaded');
+
+    // const closetimeout = setTimeout(() => )
+  }, 4200);
+
   return (
     <div className="loader">
-      <div />
-      <div />
-      <div />
+      <div className={itemClassName} />
+      <div className={itemClassName}>
+        {
+          itemClassName.length === 0
+            ? ''
+            : <svg width="150" height="150">
+                <path id="check" d="M2, 6 l3, 2.5 l4.5, -4.5" />
+              </svg>
+
+        }
+      </div>
+      <div className={itemClassName} />
     </div>
   );
 };
