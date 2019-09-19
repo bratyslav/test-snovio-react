cea379a03d777804e3dadd8743ff837849472690
bratyslav <beginner.altavista@gmail.com>
2019-09-19 13:14:53 +0300

    save content items

--- src/components/ContentItem.js
+++ src/components/ContentItem.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ContentItem = ({ post }) => {
+  const [isHover, toggleHover] = useState(false);
+
   return (
-    <div className="content-item">
-      <div className="content-item__title-wrapper">
-        <h3 className="content-item__title">{post.title}</h3>
+    <div  className="content-item__wrapper">
+      <div
+        className="content-item"
+        onMouseOver={() => toggleHover(true)}
+        onMouseLeave={() => toggleHover(false)}
+      >
+        <div className="content-item__title-wrapper">
+          <h3 className="content-item__title">{post.title}</h3>
+        </div>
+        <div>
+          {post.body}
+        </div>
+        <div>
+          <svg className="comments-icon"><g><ellipse id="ellipse" cx="6" cy="6" rx="6" ry="5" /><path id="triangle" d="M2 9, 4 10, 0 13" /></g></svg>
+          <span className="content-item__comments-count">{post.comments.length}</span>
+        </div>
       </div>
-      <div>
-        {post.body}
-      </div>
-      <div>
-        <svg className="comments-icon"><g><ellipse id="ellipse" cx="6" cy="6" rx="6" ry="5" /><path id="triangle" d="M2 9, 4 10, 0 13" /></g></svg>
+
+      <div
+        className="content-item-active"
+        onMouseOver={() => toggleHover(true)}
+        onMouseLeave={() => toggleHover(false)}
+        style={isHover ? {"display": "block"} : {"display": "none"}}
+      >
+        <div className="content-item__title-wrapper">
+          <h3 className="content-item__title">{post.title}</h3>
+        </div>
+        <div>
+          {post.body}
+        </div>
+        <div>
+          <svg className="comments-icon"><g><ellipse id="ellipse" cx="6" cy="6" rx="6" ry="5" /><path id="triangle" d="M2 9, 4 10, 0 13" /></g></svg>
+          <span className="content-item__comments-count">{post.comments.length}</span>
+        </div>
+
+        <div className="content-item-active__comments-header">Comments</div>
+
+        {
+          post.comments.map(comment => (
+            <div className="content-item-active__comment">{comment} <hr /></div>
+          ))
+        }
       </div>
     </div>
   );
